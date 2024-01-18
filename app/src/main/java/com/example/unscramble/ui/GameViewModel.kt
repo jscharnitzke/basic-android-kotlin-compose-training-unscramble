422c371152247ec42c9356c94db70f682a13603a
Joe Scharnitzke <j.scharnitzke@gmail.com>
2024-01-18 17:07:16 -0500

    add persistent state values and helper methods

--- app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -1,13 +1,47 @@
 package com.example.unscramble.ui
 
 import androidx.lifecycle.ViewModel
+import com.example.unscramble.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class GameViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(GameUiState())
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf()
 
     val uiState: StateFlow<GameUiState>
         get() = _uiState.asStateFlow()
+
+    init {
+        resetGame()
+    }
+
+    private fun pickRandomWordAndShuffle(): String {
+        currentWord = allWords.random()
+
+        if(usedWords.contains(currentWord)) {
+            return pickRandomWordAndShuffle()
+        } else {
+            usedWords.add(currentWord)
+            return shuffleCurrentWord(currentWord)
+        }
+    }
+
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
+    }
+
+    private fun shuffleCurrentWord(word: String): String {
+        val chars = word.toCharArray()
+
+        chars.shuffle()
+        while(String(chars) == word) {
+            chars.shuffle()
+        }
+
+        return String(chars)
+    }
 }
